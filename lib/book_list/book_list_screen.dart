ec141daf10d316f285c0f73815fd543a6735920e
fulmoon <63829420+fulmoon@users.noreply.github.com>
2022-08-09 01:35:56 +0900

    Feat: add updating book    onMini

--- lib/book_list/book_list_screen.dart
+++ lib/book_list/book_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:book_list_app/add_book/add_book_screen.dart';
+import 'package:book_list_app/update_book/update_book_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:book_list_app/book_list/book_list_view_model.dart';
 import 'package:flutter/material.dart';
@@ -43,6 +44,10 @@ class BookListScreen extends StatelessWidget {
                   ),
                   key: ValueKey(document.id),
                   child: ListTile(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => UpdateBookScreen(document)),
+                      );
+                    },
                     title: Text(data['title']),
                     subtitle: Text(data['author']),
                   ),
